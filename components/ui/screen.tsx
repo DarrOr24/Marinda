de17c28b711d8e67b1dbd2662981df2fecb18dbd
Darr Or <darrmorgan@gmail.com>
2026-02-04 19:12:11 -0500

    Add bottomOffset support to Screen component

--- components/ui/screen.tsx
+++ components/ui/screen.tsx
@@ -7,14 +7,14 @@ import CheckerboardBackground from '../checkerboard-background'
 type Props = {
   children: React.ReactNode
   gap?: 'no' | 'sm' | 'md' | 'lg'
+  bottomOffset?: number
 }
 
-export function Screen({ children, gap = 'md' }: Props) {
+export function Screen({ children, gap = 'md', bottomOffset = 0 }: Props) {
   const insets = useSafeAreaInsets()
 
   const gapStyle = gap === 'no' ? 0 : gap === 'sm' ? 8 : gap === 'md' ? 16 : gap === 'lg' ? 24 : 16
-  const paddingBottom = 24 + insets.bottom
-
+  const paddingBottom = 24 + insets.bottom + bottomOffset
   return (
     <SafeAreaView style={styles.screen} edges={['left', 'right', 'bottom']}>
       <CheckerboardBackground colorA="#F6FAFF" colorB="#EAF3FF" size={28} />
